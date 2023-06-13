d19af440fffe93cf119d06725369df3b206729ef
Kyle M Hall <kyle@bywatersolutions.com>
2023-06-13 08:48:12 -0400

    Initial commit

---
+++ Koha/Plugin/Com/ByWaterSolutions/TwilioSMS.pm
@@ -0,0 +1,143 @@
+package Koha::Plugin::Com::ByWaterSolutions::TwilioSMS;
+
+## It's good practice to use Modern::Perl
+use Modern::Perl;
+
+## Required for all plugins
+use base qw(Koha::Plugins::Base);
+
+## We will also need to include any Koha libraries we want to access
+use C4::Auth;
+use C4::Context;
+
+use Koha::Account::Lines;
+use Koha::Account;
+use Koha::DateUtils;
+use Koha::Libraries;
+use Koha::Patron::Categories;
+use Koha::Patron;
+
+use Cwd qw(abs_path);
+use Data::Dumper;
+use LWP::UserAgent;
+use MARC::Record;
+use Mojo::JSON qw(decode_json);;
+use URI::Escape qw(uri_unescape);
+
+## Here we set our plugin version
+our $VERSION = "{VERSION}";
+our $MINIMUM_VERSION = "{MINIMUM_VERSION}";
+
+## Here is our metadata, some keys are required, some are optional
+our $metadata = {
+    name            => 'Twilio Interactive SMS',
+    author          => 'Kyle M Hall',
+    date_authored   => '2023-06-13',
+    date_updated    => "1900-01-01",
+    minimum_version => $MINIMUM_VERSION,
+    maximum_version => undef,
+    version         => $VERSION,
+    description     => 'This plugin enables patrons to send sms messages to Koha and recieve responses.',
+};
+
+## This is the minimum code required for a plugin's 'new' method
+## More can be added, but none should be removed
+sub new {
+    my ( $class, $args ) = @_;
+
+    ## We need to add our metadata here so our base class can access it
+    $args->{'metadata'} = $metadata;
+    $args->{'metadata'}->{'class'} = $class;
+
+    ## Here, we call the 'new' method for our base class
+    ## This runs some additional magic and checking
+    ## and returns our actual $self
+    my $self = $class->SUPER::new($args);
+
+    return $self;
+}
+
+## If your tool is complicated enough to needs it's own setting/configuration
+## you will want to add a 'configure' method to your plugin like so.
+## Here I am throwing all the logic into the 'configure' method, but it could
+## be split up like the 'report' method is.
+sub configure {
+    my ( $self, $args ) = @_;
+    my $cgi = $self->{'cgi'};
+
+    unless ( $cgi->param('save') ) {
+        my $template = $self->get_template({ file => 'configure.tt' });
+
+        ## Grab the values we already have for our settings, if any exist
+        $template->param(
+            enable_opac_payments => $self->retrieve_data('enable_opac_payments'),
+            foo             => $self->retrieve_data('foo'),
+            bar             => $self->retrieve_data('bar'),
+            last_upgraded   => $self->retrieve_data('last_upgraded'),
+        );
+
+        $self->output_html( $template->output() );
+    }
+    else {
+        $self->store_data(
+            {
+                enable_opac_payments => $cgi->param('enable_opac_payments'),
+                foo                => $cgi->param('foo'),
+                bar                => $cgi->param('bar'),
+                last_configured_by => C4::Context->userenv->{'number'},
+            }
+        );
+        $self->go_home();
+    }
+}
+
+## This is the 'install' method. Any database tables or other setup that should
+## be done when the plugin if first installed should be executed in this method.
+## The installation method should always return true if the installation succeeded
+## or false if it failed.
+sub install() {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+## This is the 'upgrade' method. It will be triggered when a newer version of a
+## plugin is installed over an existing older version of a plugin
+sub upgrade {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+## This method will be run just before the plugin files are deleted
+## when a plugin is uninstalled. It is good practice to clean up
+## after ourselves!
+sub uninstall() {
+    my ( $self, $args ) = @_;
+
+    return 1;
+}
+
+## API methods
+# If your plugin implements API routes, then the 'api_routes' method needs
+# to be implemented, returning valid OpenAPI 2.0 paths serialized as a hashref.
+# It is a good practice to actually write OpenAPI 2.0 path specs in JSON on the
+# plugin and read it here. This allows to use the spec for mainline Koha later,
+# thus making this a good prototyping tool.
+
+sub api_routes {
+    my ( $self, $args ) = @_;
+
+    my $spec_str = $self->mbf_read('openapi.json');
+    my $spec     = decode_json($spec_str);
+
+    return $spec;
+}
+
+sub api_namespace {
+    my ( $self ) = @_;
+    
+    return 'twiliosms';
+}
+
+1;
---
+++ README.md
@@ -0,0 +1,17 @@
+# Introduction
+
+The Twilio SMS plugin for Koha allows patrons to send text messages to Koha and recieve responses in turn.
+
+Actions that can be performed via SMS include:
+* List current checkouts ( all overdue, or everything )
+* List current fees and fines owed
+* List waiting holds
+* View list of keywords
+* Renewing items ( either specific, all overdue, or everything )
+* Change patron's preferred language
+* Change SMS phone number
+
+
+# Downloading
+
+From the [release page](https://github.com/bywatersolutions/koha-plugin-kitchen-sink/releases) you can download the relevant *.kpz file
