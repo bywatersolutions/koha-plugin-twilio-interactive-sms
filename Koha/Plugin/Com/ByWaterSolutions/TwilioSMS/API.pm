2ff01d66431bc068313d67765a8475df199e5f26
Kyle Hall <kyle@bywatersolutions.com>
2023-06-21 16:59:28 +0000

    Implement TEST, HELPME, and command not found

--- Koha/Plugin/Com/ByWaterSolutions/TwilioSMS/API.pm
+++ Koha/Plugin/Com/ByWaterSolutions/TwilioSMS/API.pm
@@ -41,7 +41,6 @@ sub webhook {
     my $incoming = $params->{Body};   # We are respnding *To* the *From* number
 
     my $token = $c->validation->param('token');
-    warn "TOKEN: $token";
 
     my $plugin = Koha::Plugin::Com::ByWaterSolutions::TwilioSMS->new();
 
@@ -50,8 +49,6 @@ sub webhook {
     my $AuthToken        = $plugin->retrieve_data('AuthToken');
     my $From             = $plugin->retrieve_data('From');
 
-    warn "STORED: $WebhookAuthToken";
-
     unless ( $token eq $WebhookAuthToken ) {
         return $c->render(
             status  => 401,
@@ -59,11 +56,57 @@ sub webhook {
         );
     }
 
-#    unless ($patron) {
-#        return $c->render( status => 404, openapi => { error => "Patron not found." } );
-#    }
+    my $body = $params->{Body};
+    my $from = $params->{From};
 
-    my $outgoing = "This is a much more\ncomplex response";
+    my $patron;
+
+    # Look for exact match first
+    unless ( $patron = Koha::Patrons->find( { smsalertnumber => $from } ) ) {
+
+        # Look for a match without the country code
+        $from =~ s/^\+1//g;    # Remove country code ( hard coded to USA atm )
+        $patron = Koha::Patrons->find( { smsalertnumber => $from } );
+    }
+
+    # TODO: move this to a TWILIO_NO_CMD template
+    my $outgoing =
+      "I didn't understand your command. Send 'HELP' for a list of commands";
+
+    my $code = "TWILIO_NO_CMD";
+    my $lang = $patron ? $patron->lang : "default";
+
+    my $tables = {};
+    $tables->{borrowers} = $patron->id if $patron;
+
+    if ( !$patron || $body =~ m/^TEST/i ) {
+        $code = "TWILIO_TEST";
+    }
+    elsif ( $body =~ m/^HELP\s*ME/i ) {
+        $code = "TWILIO_HELP";
+    }
+    else {
+        $code = "TWILIO_NO_CMD";
+    }
+
+    my $template =
+      Koha::Notice::Templates->find( { code => $code, lang => $lang } );
+    my $template_content = $template->content;
+    warn "TABLES: " . Data::Dumper::Dumper($tables);
+    my $letter = C4::Letters::GetPreparedLetter(
+        module      => 'circulation',
+        letter_code => $code,
+        lang        => $lang,
+        tables      => $tables,
+        objects     => {
+            nothing => undef # Placeholder in case $tables is empty
+        },
+        message_transport_type => 'sms'
+    );
+    warn "LETTER: " . Data::Dumper::Dumper($letter);
+    $outgoing = $letter->{content};
+
+    warn "OUTGOING: $outgoing";
 
     my $ua = LWP::UserAgent->new;
     my $url =
@@ -79,9 +122,10 @@ sub webhook {
 
     if ( $response->is_success ) {
         warn "RESPONSE: " . $response->decoded_content;
-    } else {
+    }
+    else {
         warn "Twilio response indicates failure: " . $response->status_line;
-    } 
+    }
 
     return $c->render(
         status  => 200,
