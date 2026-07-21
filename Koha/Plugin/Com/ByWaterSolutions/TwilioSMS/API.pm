4bd28345f5aa09f6fe2c014fcbdc3303d0cdf165
Kyle M Hall <kyle@bywatersolutions.com>
2026-07-21 14:43:15 -0400

    Interactive SMS replies fail if the response contains characters outside of Latin-1
    This patch makes the webhook reply loop encode message bodies as UTF-8
    before posting them to Twilio, so replies containing characters like an
    en dash in a title don't die in the request encoding.
    
    It also changes the chunking back to a regex, this time with the /s
    modifier so newlines can't break it. The unpack approach strips
    trailing whitespace from every chunk, so a chunk boundary landing on a
    space or newline silently deleted it and jammed words together.

--- Koha/Plugin/Com/ByWaterSolutions/TwilioSMS/API.pm
+++ Koha/Plugin/Com/ByWaterSolutions/TwilioSMS/API.pm
@@ -20,6 +20,7 @@ use Modern::Perl;
 use Mojo::Base 'Mojolicious::Controller';
 
 use Data::Dumper;
+use Encode;
 use HTTP::Request::Common;
 use LWP::UserAgent;
 use Mojo::JSON qw(decode_json);
@@ -256,14 +257,14 @@ sub webhook {
     # Twilio cannot auto-split messages over 1600 characters, so we need to split
     # our message into 1600 character chunks and send each one separately.
     # Twilio should then split those messages into 160 character chunks as well
-    my @chunks = unpack( "(A1500)*", $outgoing );
+    my @chunks = $outgoing =~ /(.{1,1600})/sg;
     warn "OUTGOING: " . Data::Dumper::Dumper( \@chunks );
 
     my $ua  = LWP::UserAgent->new;
     my $url = "https://api.twilio.com/2010-04-01/Accounts/$AccountSid/Messages.json";
 
     for my $chunk (@chunks) {
-        my $request = POST $url, [ From => $From, To => $To, Body => $chunk ];
+        my $request = POST $url, [ From => $From, To => $To, Body => Encode::encode_utf8($chunk) ];
         $request->authorization_basic( $AccountSid, $AuthToken );
         my $response = $ua->request($request);
 
